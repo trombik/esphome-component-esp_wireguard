09bbf873af7f03f68c84304baa72ba258edda4d7
Tomoyuki Sakurai <y@trombik.org>
2022-06-17 18:53:22 +0700

    bugfix: lint flake8

--- components/wireguard/__init__.py
+++ components/wireguard/__init__.py
@@ -16,7 +16,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.components import sensor
 from esphome.const import CONF_ID, ICON_WIFI
 
 AUTO_LOAD = ["text_sensor"]
